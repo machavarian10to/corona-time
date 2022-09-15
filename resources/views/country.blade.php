e91a9f72080623d60381321d5ec10476d8594b45
machavarian10to <otar.matchavariani.1@btu.edu.ge>
2022-09-15 17:35:12 +0400

    feat: create search and sort functional

--- resources/views/country.blade.php
+++ resources/views/country.blade.php
@@ -1,3 +1,87 @@
 <x-layout>
-    country
+    <x-dashboard-template title="Statistics by country">
+        <div class="border-b my-[50px]">
+            <div class="flex w-[250px] justify-between ">
+                <a href="{{ route('dashboard') }}">
+                    <p>Worldwide</p>
+                </a>
+                <p class="font-bold border-b-4 border-b-black h-[40px]">
+                    By country
+                </p>
+            </div>
+        </div>
+
+        <form method="GET" >
+            <div class="mb-16 relative">
+                <div class="flex absolute inset-y-0 left-1
+                items-center pl-3 pointer-events-none">
+                    <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+                </div>
+
+                <input
+                    type="text"
+                    id="search"
+                    name="search"
+                    class="pl-12 w-[240px] h-[48px] rounded-lg border
+                    border-gray-300 text-sm focus:border-none"
+                    placeholder="Search by country"
+                    value="{{ request('search') }}"
+                >
+            </div>
+        </form>
+
+        <div class="overflow-hidden overflow-y-scroll h-[600px] mb-16">
+            <table class="table-auto border-collapse  w-full">
+                <thead class="h-[60px] top-0 z-2 sticky">
+                    <tr class="bg-table-header font-semibold text-sm ">
+                        <th>
+                            <x-table-header
+                                name="Location"
+                                value="country"
+                                sort="{{ 'country' }}"
+                            />
+                        </th>
+                        <th>
+                            <x-table-header
+                                name="New cases"
+                                value="confirmed"
+                                sort="{{ 'confirmed' }}"
+                            />
+                        </th>
+                        <th>
+                            <x-table-header
+                                name="Deaths"
+                                value="deaths"
+                                sort="{{ 'deaths' }}"
+                            />
+                        </th>
+                        <th>
+                            <x-table-header
+                                name="Recovered"
+                                value="recovered"
+                                sort="{{ 'recovered' }}"
+                            />
+                        </th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr class="border-[1px] text-center h-[50px]">
+                        <td>Worldwide</td>
+                        <td>{{ $general->pluck('confirmed')->sum() }}</td>
+                        <td>{{ $general->pluck('deaths')->sum() }}</td>
+                        <td>{{ $general->pluck('recovered')->sum() }}</td>
+                    </tr>
+                    @foreach($statistics as $statistic)
+                        <tr class="border-[1px] text-center h-[50px]">
+                            <td>{{ $statistic->country }}</td>
+                            <td>{{ $statistic->confirmed }}</td>
+                            <td>{{ $statistic->deaths }}</td>
+                            <td>{{ $statistic->recovered }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </x-dashboard-template>
 </x-layout>
