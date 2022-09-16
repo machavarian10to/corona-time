01a4226c23000e54b9b1fe2b87c3be28933bdfe7
machavarian10to <otar.matchavariani.1@btu.edu.ge>
2022-09-16 14:08:49 +0400

    fix: verification notice url

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::controller(RegisterController::class)->group(function () {
 	Route::post('/register', 'register')->name('user.register')->middleware('guest');
-	Route::get('/email/verify/{id}/{hash}', 'verify')->name('verification.verify');
+	Route::get('/email-verify/{id}/{hash}', 'verify')->name('verification.verify');
 });
 
 Route::controller(LoginController::class)->group(function () {
